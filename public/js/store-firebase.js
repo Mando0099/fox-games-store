3def76c0f8542a5301d532301bb622eb9964d111
Mando0099 <namy9585@gmail.com>
2026-06-18 23:28:05 +0300

    Update store-firebase.js

--- public/js/store-firebase.js
+++ public/js/store-firebase.js
@@ -3,45 +3,44 @@
     if(!firebase.apps.length) return;
 
     const db = firebase.firestore();
-    const snap = await db.collection('products').get();
 
-    if(snap.empty) return;
+    const snap = await db.collection('products').get();
 
     products = snap.docs.map(d => {
       const p = d.data();
 
       return {
         id: d.id,
-        name: p.name || p.title || 'Product',
-        category: p.category || p.game || 'Gaming',
-        desc: p.description || `${p.amount || ''} digital top-up`,
+        name: p.name  p.title  'Product',
+        category: p.category  p.game  'Gaming',
+        desc: p.description || ${p.amount || ''} digital top-up,
         price: Number(p.price || 0),
-        bg: p.bg || '/assets/bg-pubg.svg',
-        img: p.image || p.img || '/assets/item-uc.svg',
+        bg: p p.image e || p.img || '/assets/bg-pubg.svg',
+        img: p.im p.img g || '/assets/item-uc.svg',
         popular: 10
       };
     });
 
     const catSnap = await db.collection('categories').get();
 
-//if (!catSnap.empty) {
-  categories = catSnap.docs.map(d => {
-    const c = d.data();
+    if(!catSnap.empty){
+      categories = catSnap.docs.map(d => {
+        const c = d.data();
 
-    return {
-      name: c.name || 'Category',
-      desc: c.description || c.desc || '',
-      bg: c.image || c.bg || '/assets/bg-pubg.svg'
-    };
-  });
-} else {
-  categories = [...new Set(products.map(p => p.category || 'Gaming'))]
-    .map(c => ({
-      name: c,
-      desc: c,
-      bg: '/assets/bg-pubg.svg'
-    }));
-}
+        return {
+          name: c.name || 'Category',
+          desc: c.descript c.desc c || '',
+          bg: c.im c.img g || c.bg || '/assets/bg-pubg.svg'
+        };
+      });
+    }else{
+      categories = [...new Set(products.map(p => p.category || 'Gaming'))]
+        .map(c => ({
+          name: c,
+          desc: c,
+          bg: '/assets/bg-pubg.svg'
+        }));
+    }
 
     renderCategories();
     renderFilters();
